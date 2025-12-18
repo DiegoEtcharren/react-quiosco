e35094bea60a7d613eae6e64059cda1f490e5907
Diego Etcharren <diegoe96@gmail.com>
2025-12-17 20:19:00 -0600

    hooks

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,14 +1,24 @@
-export default function Categoria({categoria}) {
+import useQuiosco from "../hooks/useQuiosco"
 
+export default function Categoria({categoria}) {
+    const {handleClickCategoria, categoriaActual } = useQuiosco();
     const {icono, id, nombre } = categoria
   return (
-    <div className="flex items-center gap-4 border border-gray-200 w-full p-3 hover:bg-amber-400 cursor-pointer">
+    <button
+      className={`flex items-center gap-4 border border-gray-200 w-full p-3 hover:bg-amber-400 cursor-pointer ${categoriaActual.id === id ? "bg-amber-400" : ""}`}
+      type="button"
+      onClick={() => handleClickCategoria(id)}
+    >
         <img
             className="w-12"
             src={`/img/icono_${icono}.svg`}
             alt="Imagen Icono"
         />
-        <p className="text-lg font-bold cursor-pointer truncate">{ nombre }</p>
-    </div>
+        <p
+          className="text-lg font-bold cursor-pointer truncate"
+        >
+          { nombre }
+        </p>
+    </button>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
+import useQuiosco from "../hooks/useQuiosco";
 import Categoria  from "./Categoria";
-import { categorias } from "../data/categorias";
+
 
 export default function Sidebar() {
+  const { categorias } = useQuiosco()
   return (
     <aside className="md:w-72">
       <div className="p-4">
@@ -11,6 +13,7 @@ export default function Sidebar() {
       <div className="mt-10">
         {categorias.map((categoria) => (
           <Categoria
+            key = {categoria.id}
             categoria = {categoria}
           />
         ))}
