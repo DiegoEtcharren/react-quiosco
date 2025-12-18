4505bcce096a65703b052d817d6e9be91b42eb80
Diego Etcharren <diegoe96@gmail.com>
2025-12-18 17:48:13 -0600

    Product List

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,5 +1,43 @@
+import useQuiosco from "../hooks/useQuiosco"
+import ResumenProducto from "./ResumenProducto";
 export default function Resumen() {
+  const { pedido } = useQuiosco();
   return (
-    <div className="md:w-72">Resumen</div>
+    <aside className="w-72 h-screen overflow-y-scroll p-5">
+      <h1 className="text-4xl font-black">
+        Mi Pedido
+      </h1>
+      <p className="text-lg my-5">
+        Aqui podrás ver el resumen y totales de tu orden.
+      </p>
+      <div className="py-10 ">
+        {pedido.length === 0 ? (
+          <p className="text-center text-2xl">
+            No hay elementos en tu carrito
+          </p>
+        ) : (
+          pedido.map(producto => (
+            <ResumenProducto
+              key={producto.id}
+              producto = {producto}
+            />
+          ))
+        )}
+      </div>
+
+      <p className="text-xl mt-10">
+        Total: {``}
+      </p>
+
+      <form className="w-full">
+        <div className="mt-5">
+          <input
+            type="submit"
+            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer"
+            value="Confirmar Pedido"
+          />
+        </div>
+      </form>
+    </aside>
   )
 }
--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -7,18 +7,44 @@ const QuioscoProvider = ({children}) => {
 
     const [categorias, setCategorias ] = useState(categoriasDB);
     const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
+    const [modal, setModal] = useState(false);
+    const [producto, setProducto] = useState({});
+    const [pedido, setPedido] = useState([]);
 
     const handleClickCategoria = (id) => {
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
         setCategoriaActual(categoria)
     }
 
+    const handleClickModal = () => {
+        setModal(!modal)
+    }
+
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+
+    const handleAgregarPedido = ({ categoria_id, imagen, ...producto }) => {
+      if (pedido.some((pedidoState) => pedidoState.id === producto.id)) {
+        const pedidoActualizado = pedido.map( pedidoState => pedidoState.id === producto.id ? producto : pedidoState)
+        setPedido(pedidoActualizado)
+      } else {
+        setPedido([...pedido, producto]);
+      }
+    };
+
     return (
         <QuioscoContext.Provider
             value = {{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria,
+                modal,
+                handleClickModal,
+                producto,
+                handleSetProducto,
+                pedido,
+                handleAgregarPedido
             }}
         >
             {children}
